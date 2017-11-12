1b62329dd9bdca9605b263a59be6c5d99e7b080b
Paul Sajna <paulsajna@gmail.com>
2017-11-12 11:41:51 -0800

    add bzip2

---
+++ recipes/bzip2/recipe.sh
@@ -0,0 +1,49 @@
+VERSION=1.0.6
+TAR=http://www.bzip.org/$VERSION/bzip2-$VERSION.tar.gz
+
+export AR="${HOST}-ar"
+export AS="${HOST}-as"
+export CC="${HOST}-gcc"
+export CXX="${HOST}-g++"
+export LD="${HOST}-ld"
+export NM="${HOST}-nm"
+export OBJCOPY="${HOST}-objcopy"
+export OBJDUMP="${HOST}-objdump"
+export RANLIB="${HOST}-ranlib"
+export READELF="${HOST}-readelf"
+export STRIP="${HOST}-strip"
+
+function recipe_version {
+    echo "$VERSION"
+    skip=1
+}
+
+function recipe_update {
+    echo "skipping update"
+    skip=1
+}
+
+function recipe_build {
+    sysroot="${PWD}/../sysroot"
+    export LDFLAGS="-L$sysroot/lib"
+    export CPPFLAGS="-I$sysroot/include"
+    make 
+    skip=1
+}
+
+function recipe_test {
+    echo "skipping test"
+    skip=1
+}
+
+function recipe_clean {
+    make clean
+    skip=1
+}
+
+function recipe_stage {
+    dest="$(realpath $1)"
+    make install PREFIX=$dest
+    rm -rf $1/share/man
+    skip=1
+}
